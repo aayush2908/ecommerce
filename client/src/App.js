bcc677d72edc4784574515fc6ef9d27fd9f859d7
Aayush Agarwal <36470108+aayush2908@users.noreply.github.com>
2020-11-23 23:52:49 +0530

    Logout implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Switch, Route } from "react-router-dom";
@@ -8,7 +8,28 @@ import Home from "./pages/Home";
 import Header from "./components/nav/Header";
 import RegisterComplete from "./pages/auth/RegisterComplete";
 
+import { auth } from "./firebase";
+import { useDispatch } from "react-redux";
+
 function App() {
+  const dispatch = useDispatch();
+
+  //to check firebase auth state
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const idTokenResult = await user.getIdTokenResult();
+        dispatch({
+          type: "LOGGED_IN_USER",
+          payload: {
+            email: user.email,
+            token: idTokenResult.token,
+          },
+        });
+      }
+    });
+    return () => unsubscribe();
+  }, []);
   return (
     <>
       <Header />
--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -5,16 +5,31 @@ import {
   UserOutlined,
   AppstoreOutlined,
   SettingOutlined,
+  LogoutOutlined,
 } from "@ant-design/icons";
-
+import firebase from "firebase";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
+
 const { SubMenu, Item } = Menu;
 
 const Header = () => {
   const [current, setCurrent] = useState("home");
+  let dispatch = useDispatch();
+  let history = useHistory();
 
   const handleClick = (e) => {
     setCurrent(e.key);
+  };
+
+  const logout = () => {
+    firebase.auth().signOut();
+    dispatch({
+      type: "LOGOUT",
+      payload: null,
+    });
+    history.push("/login");
   };
 
   return (
@@ -29,8 +44,11 @@ const Header = () => {
         <Link to="/login">Login</Link>
       </Item>
       <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Username">
-        <Menu.Item key="setting:1">Option 1</Menu.Item>
-        <Menu.Item key="setting:2">Option 2</Menu.Item>
+        <Item key="setting:1">Option 1</Item>
+        <Item key="setting:2">Option 2</Item>
+        <Item icon={<LogoutOutlined />} onClick={logout}>
+          Logout
+        </Item>
       </SubMenu>
     </Menu>
   );
