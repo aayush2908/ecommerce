e95526d4403363383d5160819b62bf33c25a4416
Aayush Agarwal <36470108+aayush2908@users.noreply.github.com>
2020-11-23 19:49:16 +0530

    Registration using Firebase auth completed

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { auth } from "../../firebase";
-import { toast, ToastContainer } from "react-toastify";
+import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const Register = () => {
@@ -8,8 +8,9 @@ const Register = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    console.log(process.env.REACT_APP_REGISTER_REDIRECT_URL);
     const config = {
-      url: "http://localhost:3000/register/complete",
+      url: process.env.REACT_APP_REGISTER_REDIRECT_URL,
       handleCodeInApp: true,
     };
     await auth.sendSignInLinkToEmail(email, config);
@@ -44,7 +45,6 @@ const Register = () => {
       <div className="row">
         <div className="col-md-6 offset-md-3">
           <h4>Register</h4>
-          <ToastContainer />
           {registerForm()}
         </div>
       </div>
