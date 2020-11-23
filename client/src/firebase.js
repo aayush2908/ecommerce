776b510de08672d1f9048879fa9fb74c62a11598
Aayush Agarwal <36470108+aayush2908@users.noreply.github.com>
2020-11-23 16:49:43 +0530

    Firebase setup added for auth

---
+++ client/src/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDUbDejcCtI2_4O4rYz3Qdt6M7s-NRjxmA",
+  authDomain: "ecommerce-7c3ea.firebaseapp.com",
+  databaseURL: "https://ecommerce-7c3ea.firebaseio.com",
+  projectId: "ecommerce-7c3ea",
+  storageBucket: "ecommerce-7c3ea.appspot.com",
+  messagingSenderId: "628801657097",
+  appId: "1:628801657097:web:0d7c08a824eb2b47f01bd8",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
