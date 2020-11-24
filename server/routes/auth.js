3ffa4b9ddf03770a8fe6d93fbebc8c702ba0c30d
Aayush Agarwal <36470108+aayush2908@users.noreply.github.com>
2020-11-25 00:30:03 +0530

    Firebase auth check server side

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+//middleware
+const { authCheck } = require("../middleware/auth");
 //import
 const { createOrUpdateUser } = require("../controllers/auth");
 
-router.get("/user", createOrUpdateUser);
+router.post("/create-or-update-user", authCheck, createOrUpdateUser);
 
 module.exports = router;
