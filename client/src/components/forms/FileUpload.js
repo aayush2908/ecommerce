c8442273d103809e5ab93304ea7abb8aba8362bd
Aayush Agarwal <36470108+aayush2908@users.noreply.github.com>
2020-12-04 18:18:15 +0530

    File(image) resize in frontend

--- client/src/components/forms/FileUpload.js
+++ client/src/components/forms/FileUpload.js
@@ -1,7 +1,28 @@
 import React from "react";
+import Resizer from "react-image-file-resizer";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
 const FileUpload = () => {
-  const fileUploadAndResize = (e) => {};
+  const { user } = useSelector((state) => ({ ...state }));
+
+  const fileUploadAndResize = (e) => {
+    let files = e.target.files;
+    if (files) {
+      for (let i = 0; i < files.length; i++) {
+        Resizer.imageFileResizer(
+          files[i],
+          720,
+          720,
+          "JPEG",
+          100,
+          0,
+          (uri) => {},
+          "base64"
+        );
+      }
+    }
+  };
 
   return (
     <div className="row">
