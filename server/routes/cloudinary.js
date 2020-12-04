f7243ec02341bd3e39f6c309e1e919396b3f56cc
Aayush Agarwal <36470108+aayush2908@users.noreply.github.com>
2020-12-04 17:57:44 +0530

    Cloudinary setup and image endpoints added

---
+++ server/routes/cloudinary.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+//middleware
+const { authCheck, adminCheck } = require("../middleware/auth");
+//import
+const { upload, remove } = require("../controllers/cloudinary");
+
+router.post("/uploadimages", authCheck, adminCheck, upload);
+router.post("/removeimages", authCheck, adminCheck, remove);
+
+module.exports = router;
