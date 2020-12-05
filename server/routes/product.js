e5a6e30d540f26a8ab912823b10b5f89d02d6613
Aayush Agarwal <36470108+aayush2908@users.noreply.github.com>
2020-12-05 15:01:22 +0530

    Product delete endpoint

--- server/routes/product.js
+++ server/routes/product.js
@@ -4,9 +4,10 @@ const router = express.Router();
 //middleware
 const { authCheck, adminCheck } = require("../middleware/auth");
 //import
-const { create, listAll } = require("../controllers/product");
+const { create, listAll, remove } = require("../controllers/product");
 
 router.post("/product", authCheck, adminCheck, create);
 router.get("/products/:count", listAll);
+router.post("/product/:slug", authCheck, adminCheck, remove);
 
 module.exports = router;
