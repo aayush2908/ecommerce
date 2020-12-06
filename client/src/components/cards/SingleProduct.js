d1a7aa3841b449d8fd547ac4ab56d331d6af673b
Aayush Agarwal <36470108+aayush2908@users.noreply.github.com>
2020-12-06 23:42:19 +0530

    View Product page with image carousel added

---
+++ client/src/components/cards/SingleProduct.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Card } from "antd";
+import { Link } from "react-router-dom";
+import { HeartOutlined, ShoppingCartOutlined } from "@ant-design/icons";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+const SingleProduct = ({ product }) => {
+  const { title, description, images, slug, price } = product;
+  const { Meta } = Card;
+
+  return (
+    <>
+      <div className="col-md-7">
+        <Carousel showArrows={true} autoPlay infiniteLoop>
+          {images && images.map((i) => <img src={i.url} key={i.public_id} />)}
+        </Carousel>
+      </div>
+      <div className="col-md-5">
+        <Card
+          actions={[
+            <>
+              <ShoppingCartOutlined className="text-success" />
+              <br /> Add to Cart
+            </>,
+            <Link to="/">
+              <HeartOutlined className="text-info" />
+              <br /> Add to Wishlist
+            </Link>,
+          ]}
+        >
+          <Meta title={title} description={description} />
+        </Card>
+      </div>
+    </>
+  );
+};
+
+export default SingleProduct;
