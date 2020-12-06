41c13f915db7b2be31b84ddd9f68ecddf6da2a73
Aayush Agarwal <36470108+aayush2908@users.noreply.github.com>
2020-12-06 16:20:08 +0530

    modified list endpoint for pagination

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -62,10 +62,32 @@ exports.update = async (req, res) => {
   }
 };
 
+//WITHOUT PAGINATION
+// exports.list = async (req, res) => {
+//   try {
+//     const { sort, order, limit } = req.body;
+//     const products = await Product.find({})
+//       .populate("category")
+//       .populate("subs")
+//       .sort([[sort, order]])
+//       .limit(limit)
+//       .exec();
+
+//     res.json(products);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+//WITH PAGINATION
 exports.list = async (req, res) => {
   try {
-    const { sort, order, limit } = req.body;
+    const { sort, order, page } = req.body;
+    const currentPage = page || 1;
+    const perPage = 3;
+
     const products = await Product.find({})
+      .skip((currentPage - 1) * perPage)
       .populate("category")
       .populate("subs")
       .sort([[sort, order]])
