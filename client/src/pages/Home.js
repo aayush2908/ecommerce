80c2628488fd935d10e23db1303aa8cad5dd51d9
Aayush Agarwal <36470108+aayush2908@users.noreply.github.com>
2020-12-06 14:46:28 +0530

    Best Selling Products Added

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Jumbotron from "../components/cards/Jumbotron";
 import NewArrivals from "../components/home/NewArrivals";
+import BestSellers from "../components/home/BestSellers";
 
 const Home = () => {
   return (
@@ -14,6 +15,12 @@ const Home = () => {
       </h4>
 
       <NewArrivals />
+      <br />
+      <h4 className="text-center p-3 mt-5 mb-5 display-3 jumbotron">
+        Best Sellers
+      </h4>
+
+      <BestSellers />
       <br />
     </>
   );
