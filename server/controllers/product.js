9444b0fda67a1d10cf1a5c185cc81ed92c73cd6f
Aayush Agarwal <36470108+aayush2908@users.noreply.github.com>
2020-12-13 19:53:16 +0530

    Filter by subcategory backend

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -215,8 +215,17 @@ const handleStar = (req, res, stars) => {
     });
 };
 
+const handleSub = async (req, res, sub) => {
+  const products = await Product.find({ subs: sub })
+    .populate('category,"_id name')
+    .populate("subs", "_id name")
+    .populate("postedBy", "_id name")
+    .exec();
+  res.json(products);
+};
+
 exports.searchFilters = async (req, res) => {
-  const { query, price, category, stars } = req.body;
+  const { query, price, category, stars, sub } = req.body;
   if (query) {
     await handleQuery(req, res, query);
   }
@@ -228,5 +237,8 @@ exports.searchFilters = async (req, res) => {
   }
   if (stars) {
     await handleStar(req, res, stars);
+  }
+  if (sub) {
+    await handleSub(req, res, sub);
   }
 };
