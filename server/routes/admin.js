7f379f9dd697028d823cf259e33cadff56c99f7d
Aayush Agarwal <36470108+aayush2908@users.noreply.github.com>
2020-12-19 12:10:14 +0530

    Order status update at admin backend

---
+++ server/routes/admin.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+//middleware
+const { authCheck, adminCheck } = require("../middleware/auth");
+const { orders, orderStatus } = require("../controllers/admin");
+
+router.get("/admin/orders", authCheck, adminCheck, orders);
+router.put("/admin/order-status", authCheck.length, adminCheck, orderStatus);
+
+module.exports = router;
