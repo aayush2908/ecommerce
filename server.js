2b2bbfad5edeeddac8fc18a27f29b2ba3336da97
Aayush Agarwal <36470108+aayush2908@users.noreply.github.com>
2020-12-22 17:49:41 +0530

    testing

--- server.js
+++ server.js
@@ -33,6 +33,10 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
 //port
 const port = process.env.PORT || 8000;
 
